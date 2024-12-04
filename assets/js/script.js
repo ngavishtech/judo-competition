753cc50c481a16ae27e532bbb753b93fae8c09d6
Nir Gavish <ngavish.tech@gmail.com>
2024-12-04 12:19:37 +0200

    updates, settings, and more...

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,16 +29,98 @@ const removeSetting = (index) => {
     renderSettingsList();
 };
 
-const addData = (entry) => {
+const addAthlete = (entry) => {
     athletes.push(entry);
     renderGroups(athletes);
     renderUserList(athletes);
 };
 
-const deleteUser = (index) => {
-    athletes.splice(index, 1); // Remove the user from the array
-    renderGroups(athletes); // Re-render groups after deletion
-    renderUserList(athletes); // Re-render the user list
+const deleteAthlete = (index) => {
+    athletes.splice(index, 1);
+    renderGroups(athletes);
+    renderUserList(athletes);
+};
+
+const editAthletePopup = (index) => {
+    const popup = document.createElement('div');
+    popup.className = 'popup';
+    popup.innerHTML = `
+        <div class="popup-content">
+            <label>Edit Athlete</label>
+            <label for="first-name-edit">First Name</label><input type="text" id="first-name-edit" value="${athletes[index].firstName || ""}" required>
+            <label for="last-name-edit">Last Name</label><input type="text" id="last-name-edit" value="${athletes[index].lastName || ""}" required>
+            <label for="gender-edit">Gender</label>
+            <select id="gender-edit" required>
+                <option value="" disabled ${!athletes[index].gender ? "selected" : ""}></option>
+                <option value="Male" ${athletes[index].gender === "Male" ? "selected" : ""}>Male</option>
+                <option value="Female" ${athletes[index].gender === "Female" ? "selected" : ""}>Female</option>
+            </select>
+            <label for="age-edit">Birth Year</label><input type="number" step="0.5" id="age-edit" value="${athletes[index].age || 0}" required>
+            <label for="grade-edit">Grade/Class</label><input type="text" id="grade-edit" value="${athletes[index].grade || ""}" required>
+            <label for="rank-edit">Rank/Belt</label><input type="text" id="rank-edit" value="${athletes[index].rank || ""}" >
+            <label for="branch-edit">Branch/City</label><input type="text" id="branch-edit" value="${athletes[index].branch || ""}" required>
+            <label for="coach-name-edit">Coach Name</label><input type="text" id="coach-name-edit" value="${athletes[index].coachName || ""}" required>
+            <label for="weight-edit">Weight (KG)</label><input type="number" step="0.01" id="weight-edit" value="${athletes[index].weight || 0}">
+            <label for="payment-edit">Payment Details</label><input type="text" id="payment-edit" value="${athletes[index].payment || ""}">
+            <label for="comments-edit">Comments</label><textarea id="comments-edit">${athletes[index].comments}</textarea>
+                        
+            <button id="edit-athlete-save-btn">Save</button>
+            <button id="edit-athlete-cancel-btn">Cancel</button>
+        </div>
+    `;
+    document.body.appendChild(popup);
+
+    // Save the changes
+    document.getElementById('edit-athlete-save-btn').addEventListener('click', () => {
+        athletes[index] = {
+            firstName: document.getElementById("first-name-edit").value.trim(),
+            lastName: document.getElementById("last-name-edit").value.trim(),
+            gender: document.getElementById("gender-edit").value.trim(),
+            age: parseFloat(document.getElementById("age-edit").value.trim()),
+            grade: document.getElementById("grade-edit").value.trim(),
+            rank: document.getElementById("rank-edit").value.trim(),
+            branch: document.getElementById("branch-edit").value.trim(),
+            coachName: document.getElementById("coach-name-edit").value.trim(),
+            weight: parseFloat(document.getElementById("weight-edit").value.trim()),
+            payment: document.getElementById("payment-edit").value.trim(),
+            comments: document.getElementById("comments-edit").value.trim(),
+        };
+        document.body.removeChild(popup);
+        renderGroups(athletes);
+        renderUserList(athletes);
+    });
+
+    // Close the popup without saving
+    document.getElementById('edit-athlete-cancel-btn').addEventListener('click', () => {
+        document.body.removeChild(popup);
+    });
+};
+
+const editWeightPopup = (index, weight) => {
+    const popup = document.createElement('div');
+    popup.className = 'popup';
+    popup.innerHTML = `
+        <div class="popup-content">
+            <label>Edit Athlete Weight</label>
+            <input type="number" step="0.01" id="edit-weight-input" value="${weight}">
+            <button id="save-btn">Save</button>
+            <button id="cancel-btn">Cancel</button>
+        </div>
+    `;
+    document.body.appendChild(popup);
+
+    // Save the changes
+    document.getElementById('save-btn').addEventListener('click', () => {
+        athletes[index].weight = document.getElementById('edit-weight-input').value.trim();
+        document.body.removeChild(popup);
+        renderGroups(athletes);
+        renderUserList(athletes);
+    });
+
+    // Close the popup without saving
+    document.getElementById('cancel-btn').addEventListener('click', () => {
+        document.body.removeChild(popup);
+    });
 };
 
 const renderUserList = (athletesList) => {
@@ -49,12 +131,20 @@ const renderUserList = (athletesList) => {
         const li = document.createElement("li");
         li.innerHTML = `
             ${entry.firstName} ${entry.lastName} (${entry.gender}) - Birth Year: ${entry.age}, Weight: ${entry.weight} KG
+            <button class="edit-weight-btn" data-index="${index}">Edit Weight</button>
+            <button class="edit-btn" data-index="${index}">Edit Athlete</button>
             <button class="delete-btn" data-index="${index}">Delete</button>
         `;
 
         li.querySelector(".delete-btn").addEventListener("click", () => {
-            deleteUser(index);
+            deleteAthlete(index);
         });
+        li.querySelector(".edit-btn").addEventListener("click", () => {
+            editAthletePopup(index);
+        });
+        li.querySelector(".edit-weight-btn").addEventListener("click", () => {
+            editWeightPopup(index, entry.weight)
+        })
 
         athleteList.appendChild(li);
     });
@@ -63,12 +153,11 @@ const renderUserList = (athletesList) => {
 const renderGroups = (athletesList) => {
     const groupedData = {};
 
-    athletesList.forEach(entry => {
+    athletesList.forEach((entry) => {
         if (!entry.weight || isNaN(entry.weight)) {
-            if (!groupedData["TO BE WEIGHTED"]) {
-                groupedData["TO BE WEIGHTED"] = [];
-            }
-            groupedData["TO BE WEIGHTED"].push(entry);
+            const groupKey = "TO BE WEIGHTED";
+            groupedData[groupKey] = groupedData[groupKey] || [];
+            groupedData[groupKey].push(entry);
             return;
         }
 
@@ -80,16 +169,35 @@ const renderGroups = (athletesList) => {
 
         if (group) {
             const groupKey = `${group.gender} | Birth Year (${group.ageMin}-${group.ageMax}) | Weight (${group.weightMin}-${group.weightMax} KG)`;
-            if (!groupedData[groupKey]) groupedData[groupKey] = [];
+            groupedData[groupKey] = groupedData[groupKey] || [];
+            groupedData[groupKey].push(entry);
+        } else {
+            const groupKey = "NO MATCHING GROUP";
+            groupedData[groupKey] = groupedData[groupKey] || [];
             groupedData[groupKey].push(entry);
         }
     });
 
     const weighInContent = document.getElementById("grouped-view");
     weighInContent.innerHTML = "";
-    Object.keys(groupedData).forEach(group => {
+
+    Object.keys(groupedData).forEach((group) => {
         const groupEl = document.createElement("div");
-        groupEl.innerHTML = `<strong>${group}</strong><ul>${groupedData[group].map(e => `<li>${e.firstName} ${e.lastName} (${e.age}, ${e.weight}, ${e.rank})</li>`).join("")}</ul>`;
+        groupEl.innerHTML = `
+        <strong>${group} |<->| Count (${groupedData[group].length})</strong>
+        <ul>
+            ${groupedData[group]
+            .map(
+                e => `<li>
+                        <input type="checkbox" data-athlete-id="${e.id}" />
+                        ${e.firstName} ${e.lastName} (${e.age}, ${e.weight}, ${e.rank})
+                        <button class="edit-weight-btn" data-index="${athletesList.indexOf(e)}">Edit Weight</button>
+                    </li>`
+            )
+            .join("")}
+        </ul>
+        `;
+        groupEl.classList.add("group");
         weighInContent.appendChild(groupEl);
     });
 };
@@ -123,6 +231,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchWeighIn = document.getElementById("weigh-in-search");
     const refreshRegistration = document.getElementById("registration-refresh-button");
     const searchRegistration = document.getElementById("registration-search");
+    const editWeightWeighIn = document.getElementById("grouped-view");
 
     // Tab switching logic
     tabs.forEach(tab => {
@@ -152,7 +261,7 @@ document.addEventListener("DOMContentLoaded", () => {
             comments: document.getElementById("comments").value.trim(),
         };
 
-        addData(entry);
+        addAthlete(entry);
         registrationForm.reset();
     });
 
@@ -186,6 +295,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     searchWeighIn.addEventListener("input", (event) => {
         filterAthletes(event.target.value);
+    });
+
+    editWeightWeighIn.addEventListener("click", (event) => {
+        if (event.target.classList.contains("edit-weight-btn")) {
+            const index = parseInt(event.target.dataset.index, 10);
+            const weight = athletes[index]?.weight || 0;
+            editWeightPopup(index, weight);
+        }
     });
 });
 
